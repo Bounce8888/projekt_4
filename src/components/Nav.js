f2bcca844f63d0fabc46cb0fa068c6efcf79f7f0
Marek D <marek.duraj88@gmail.com>
2022-12-15 19:16:00 +0100

    poprawka

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,13 +4,13 @@ const Nav = () => (
       <div className="company-logo">nazwa firmy</div>
       <ul className="menu">
         <li>
-          <a href="#about-us">o nas</a>
+          <a href="/#about-us">o nas</a>
         </li>
         <li>
-          <a href="#our-services">oferta</a>
+          <a href="/#our-services">oferta</a>
         </li>
         <li>
-          <a>kontakt</a>
+          <a href="/#">kontakt</a>
         </li>
       </ul>
     </div>
